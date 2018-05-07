41a09c9e4f5fd318d0a98bf9d9e8befc6af5b2cf
Anísio Neto <neto.df.pn@gmail.com>
2018-05-07 16:58:32 -0300

    Migrations

---
+++ database/migrations/2018_05_06_212037_create_biddings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBiddingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biddings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('object')->nullable();
+            $table->string('date_open')->nullable();
+            $table->string('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('biddings');
+    }
+}
